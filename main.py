c189bfe5fcfcf837a6fc3f1f31af1254605b6671
Karol <ka.zmorski@gmail.com>
2024-02-14 15:50:13 +0100

    Delete feature

--- main.py
+++ main.py
@@ -106,7 +106,13 @@ def edit_post(post_id):
     return render_template("make-post.html", form=edit_form, edit=True)
 
 
-# TODO: delete_post() to remove a blog post from the database
+@app.route('/delete-post/<int:post_id>', methods=["GET", "POST"])
+def delete_post(post_id):
+    post = db.get_or_404(BlogPost, post_id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect(url_for("get_all_posts"))
+
 
 # Below is the code from previous lessons. No changes needed.
 @app.route("/about")
